4dadcdd9b5bd10df876338bd876caa8ffef711d6
Sia <sahornbuckle17@sias-macbook-pro.local>
2019-01-19 17:44:26 -0500

    Initial Commit

---
+++ calc_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calc_app
+//
+//  Created by Sia on 1/19/19.
+//  Copyright © 2019 Sia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
